496b046b75813e58767f66d868c491d4669a2458
Mike-04 <hulubeacmihai@gmail.com>
2025-03-03 12:52:15 +0200

    gradele stuff

--- src/build.gradle.kts
+++ src/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "com"
@@ -10,10 +11,25 @@ repositories {
 }
 
 dependencies {
+    implementation("com.google.guava:guava:33.4.0-jre")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+application {
+    mainClass.set("com.Main")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "com.Main"
+    }
+    from({
+        configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }
+    })
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
